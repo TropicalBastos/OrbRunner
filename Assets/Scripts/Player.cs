06e05734f9241a6060d91b71259fb6bde991f83d
TropicalBastos <ian-bastos@live.com>
2021-03-29 21:55:23 +0100

    simplification of controls

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -17,28 +17,26 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
         Vector3 cameraForward = new Vector3(cameraPivotTransform.forward.normalized.x, 0, cameraPivotTransform.forward.normalized.z);
 
-        if (moveHorizontal == 0 && moveVertical == 0) 
+        if (moveVertical == 0) 
         {
             playerRigid.velocity = Vector3.zero;
         } 
         else 
         {
             // Movement
-            Vector3 forwardMovement = cameraForward.normalized * moveVertical;
-            Vector3 sidewaysMovement = cameraForward.normalized * moveHorizontal;
-            Vector3 movement = forwardMovement + sidewaysMovement;
+            Vector3 movement = cameraForward.normalized * moveVertical;
             Vector3 force = movement * speed;
             playerRigid.AddForce(force);
 
             // Rotate forward direction
             Vector3 newForwardDirection = new Vector3(movement.normalized.x, 0, movement.normalized.z);
 
-            if (moveVertical < 0 && Mathf.Abs(moveVertical) > moveHorizontal) 
+            // If we are moving backwards then have the car's direction in the opposite of the force
+            if (moveVertical < 0) 
             {
                 transform.forward = Vector3.Lerp(transform.forward, -newForwardDirection, Time.deltaTime * speed);
             }
