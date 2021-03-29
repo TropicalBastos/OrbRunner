1ec9670b86e0581c8ac28e245558bce72fc17cac
TropicalBastos <ian-bastos@live.com>
2021-03-29 20:03:30 +0100

    reversing car forward vector fix

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -22,18 +22,30 @@ public class Player : MonoBehaviour
 
         Vector3 cameraForward = new Vector3(cameraPivotTransform.forward.normalized.x, 0, cameraPivotTransform.forward.normalized.z);
 
-        if (moveHorizontal == 0 && moveVertical == 0) {
+        if (moveHorizontal == 0 && moveVertical == 0) 
+        {
             playerRigid.velocity = Vector3.zero;
-        } else {
+        } 
+        else 
+        {
             // Movement
             Vector3 forwardMovement = cameraForward.normalized * moveVertical;
             Vector3 sidewaysMovement = cameraForward.normalized * moveHorizontal;
             Vector3 movement = forwardMovement + sidewaysMovement;
-            playerRigid.AddForce(movement * speed);
+            Vector3 force = movement * speed;
+            playerRigid.AddForce(force);
 
             // Rotate forward direction
             Vector3 newForwardDirection = new Vector3(movement.normalized.x, 0, movement.normalized.z);
-            transform.forward = Vector3.Lerp(transform.forward, newForwardDirection, Time.deltaTime * speed);
+
+            if (moveVertical < 0 && Mathf.Abs(moveVertical) > moveHorizontal) 
+            {
+                transform.forward = Vector3.Lerp(transform.forward, -newForwardDirection, Time.deltaTime * speed);
+            }
+            else
+            {
+                transform.forward = Vector3.Lerp(transform.forward, newForwardDirection, Time.deltaTime * speed);
+            }
         }
 
         // transform.forward = Vector3.Lerp(transform.forward, cameraForward, Time.deltaTime);
