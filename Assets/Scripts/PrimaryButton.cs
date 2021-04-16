6101b1cd9b8f63635f64d182da5f12207c06bf57
TropicalBastos <ian-bastos@live.com>
2021-04-16 23:04:10 +0100

    UI reskin

---
+++ Assets/Scripts/PrimaryButton.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class PrimaryButton : MonoBehaviour, IPointerDownHandler
+{
+    public Sprite onClickSprite;
+
+    private Image sourceImage;
+    private Sprite originalSprite;
+    private Button button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sourceImage = GetComponent<Image>();
+        originalSprite = sourceImage.sprite;
+        button = GetComponent<Button>();
+        button.onClick.AddListener(OnClick);
+    }
+
+    public void OnPointerDown(PointerEventData data)
+    {
+        sourceImage.sprite = onClickSprite;
+    }
+
+    private void OnClick()
+    {
+        sourceImage.sprite = originalSprite;
+    }
+}
